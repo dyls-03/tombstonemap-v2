3d4a1d8d7bef278e4135e5fdd3876171028721cd
Dylan <57984520+dyls-03@users.noreply.github.com>
2026-05-21 20:19:51 +0100

    #27: Added script (#77)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,13 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script 
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4812024080400657"
+          crossOrigin="anonymous"
+          />
+
         {children}
         <Analytics/>
         </body>
