119a6e6c00f04f056775425dd7bd2f9c1d33ca59
Dylan <57984520+dyls-03@users.noreply.github.com>
2026-05-21 23:53:57 +0100

    #75: Added a floating footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
 import Script from "next/script";
+import FloatingFooter from "@/components/layout/FloatingFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
 
         {children}
         <Analytics/>
+        <FloatingFooter />
         </body>
     </html>
   );
